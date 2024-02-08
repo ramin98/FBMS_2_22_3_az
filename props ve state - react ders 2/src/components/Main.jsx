bd80420d2323b59b5035f5ee70e2a96867765e45
ramin98 <59912261+ramin98@users.noreply.github.com>
2024-02-08 12:04:52 +0400

    Update Main.jsx

--- props ve state - react ders 2/src/components/Main.jsx
+++ props ve state - react ders 2/src/components/Main.jsx
@@ -1,16 +1,31 @@
 import GoodsItem from "./GoodsItem";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function Main() {
+  let h1 = useRef(null)
   let [value, setValue] = useState([1,2,3,4,5,6,7])
-
+  let [inputValue, setInputValue] = useState({
+    value1:'',
+    value2:'',
+    value3:'',
+  })
+  
   const plusNum = () => { 
     setValue(array => [...array, array.at(-1) + 1])
+    console.log(h1.current)
+    h1.current.style = 'color:red'
   }
   
   return (
     <main>
-      <h1>MAIN</h1>
+      <h1 ref={h1}>MAIN</h1>
+      <input onChange={(ev) => setInputValue({...inputValue,value1:ev.target.value})} type="text" />
+      <input onChange={(ev) => setInputValue({...inputValue,value2:ev.target.value})} type="text" />
+      <input onChange={(ev) => setInputValue({...inputValue,value3:ev.target.value})} type="text" />
+
+      <p>{inputValue.value1}</p>
+      <p>{inputValue.value2}</p>
+      <p>{inputValue.value3}</p>
       <button onClick={() => plusNum()}>PLUS</button>
       <div>{value}</div>
       <ul>
@@ -25,3 +40,4 @@ function Main() {
 }
 
 export default Main;
+
